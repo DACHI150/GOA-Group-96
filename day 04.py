57746cb8e5739e7cfb648ae91b1b747f0af0ff16
DACHI150 <dachidvalidze10@gmail.com>
2026-02-15 20:16:37 +0400

    commit

---
+++ day 04.py
@@ -0,0 +1,78 @@
+#user 1
+#4user
+#user/1
+
+
+
+#Debugging-ი ეწოდება პროგრამაში არსებული შეცდომის პოვნას და გამოსწორებას
+
+#Python-ში ძირითადად ვხვდებით ამ სახის Errors:Syntax Errors, Runtime Errors (ან Exceptions), და Logic Errors. 
+
+
+
+#name = "Lika"
+#print(namme)
+
+name = "Lika"
+print(name)
+
+#number = 5
+#text = " apples"
+#result = number + text
+#print(result)
+ 
+number = 5
+text = "apples"
+result = str (number)  + text
+print(result)
+
+
+#name = liKa
+#name2 = naame + "4"
+name = "liKa"
+name2 = name + "4"
+
+#first_user = "Lika"
+#2nduser = "Giorgi"
+#print(first user)
+
+first_user = "Lika"
+seconed_user = "Giorgi" 
+print(first_user)
+
+#first name = Data
+#last % name = "random"
+first_name = "Data"
+last_name = "random"
+
+
+
+#first_user = "Lika"
+#2nduser = "Giorgi"
+#print(first user)
+
+first_user = "Lika"
+seconed_user = "Giorgi"
+print(first_user)
+
+
+city = input("enter your city: " )
+district = input("enter your district: " )
+building = input("enter your building: " )
+floor = input("enter your floor: " )
+
+print(city + " " + district + " " + building + " " + floor)
+
+
+age= input("enter your age: " )
+
+print(f"You are {age} years old")
+
+
+
+Num1 = input ("enter number: " )
+Num2 = input ("enter number: " )
+
+print(Num1 + Num2)
+
+#ალბათ იმიტომ გამოიტანა 202 რომ Num1 და Num2 შეიცავს  String ის ტიპის მნიშვნელობას
---
+++ level 04/classwork/day 04.py
@@ -0,0 +1,18 @@
+#1user = "Nika"
+#2_user = "Lika"
+#daprint(2_USer)
+#ცვლადის წინ არ შეიძლება რიცხვის დაწერა
+user1 = "Nika"
+user_2 = "Lika"
+print (user_2)
+
+
+name = input('Enter your name: ') 
+surname = input ("Enter your surname: " )
+addres = input("enter your addres : ")
+age = input('Enter your age: ')
+
+print( input("i am  {name} {surname},i live in{addres} and i am {age} years old"))
+
+
+
