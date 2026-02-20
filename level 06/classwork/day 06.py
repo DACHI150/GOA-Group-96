673f89056892673f7d1e09846c8d6030027d40e4
DACHI150 <dachidvalidze10@gmail.com>
2026-02-20 19:53:15 +0400

    commit

---
+++ level 06/classwork/day 06.py
@@ -0,0 +1,22 @@
+num1=int(input("enter number"))
+num2=int(input("enter number"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 * num2)
+print(num1 ** num2)
+
+
+
+print(bool(int(True) - int(False) + int(False))) # რადგანნ True=1ს  false=0 
+ #1 - 0 + 0 =1   
+
+
+
+#true or false
+#int, float , string bool 
+
+
---
+++ level 06/homework/day 06.py
@@ -0,0 +1,63 @@
+#N2
+
+#type-ის საშვალებით შეგვიძლია გავიგოთ თუ რა ტიპის ცვლადია
+
+num1=5
+print(type(num1))
+num2=5.0
+print(type(num2))
+name="dachi"
+print(type(name))
+num3=12
+print(type(num3))
+name2= "nikolozi"
+print(type(name2))
+
+
+
+#N3
+name3="dachi"
+surname="dvalidze"
+age= 16
+
+print("i am " + name3 + " " + surname + " and i am " + str( age )+  " years old " )
+
+#N4
+number1 =float(input("enter number: " ))
+number2 =float(input("enter number: " ))
+
+print(number1 + number2 )
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+#N5
+
+number3 = int(input("enter number: "))
+number4 = int(input("enter number: "))
+number5 = int(input("enter number: "))
+number6 = int(input("enter number: "))
+number7 = int(input("enter number: "))
+
+
+number8 = number3 + number4 + number5 + number6 + number7  
+number9 = number8 // 5
+
+print(number9)
+
+#N6
+temperature = int(input("enter temperature:"))
+
+fahrenheit = temperature *  1.8 + 32 
+print(fahrenheit)
+
+#N7
+
+fahrenheit1 = int(input("enter temperature:"))
+celsius = fahrenheit1 - 32 / 1.8 
+print(celsius)
+
+
+
+
+
